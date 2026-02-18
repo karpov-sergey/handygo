0786e7fe32731390e2cc9baf5f578897c1232e73
karpov <s.i.karpoff@gmail.com>
2026-02-18 18:10:06 +0100

    structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,14 +2,25 @@
 
 <template>
 	<section class="py-10">
-		<UContainer>
-			<div class="text-center space-y-4 py-10">
-				<h1 class="text-3xl font-bold">HandyGo</h1>
-				<p class="text-gray-500">
-					Добро пожаловать! Найдите подходящее предложение.
-				</p>
-				<UButton>Button</UButton>
-			</div>
-		</UContainer>
+		<div class="text-center space-y-4 py-10">
+			<h1 class="text-3xl font-bold">HandyGo</h1>
+			<p class="text-gray-500">
+				Добро пожаловать! Найдите подходящее предложение.
+			</p>
+			<UButton>Button</UButton>
+			<UColorModeButton />
+			<UFileUpload
+				position="inside"
+				layout="list"
+				multiple
+				label="Drop your images here"
+				description="SVG, PNG, JPG or GIF (max. 2MB)"
+				class="w-96"
+				:ui="{
+					base: 'min-h-48',
+				}"
+			/>
+			<UIcon name="i-lucide-lightbulb" class="size-14 bg-yellow-400" />
+		</div>
 	</section>
 </template>
